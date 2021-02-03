a55b0b6de195f064c47c69f8f6cbad14d55ebfa1
yosoyjacko <jorge_a.correa@hotmail.com>
2021-02-02 22:04:02 -0300

    home falta editar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import {View,Text,Button,StyleSheet} from 'react-native'
+import {View,Text,Button,StyleSheet, Image} from 'react-native'
 
 
 
 
 export default function HomeScreen({navigation}) {
   return (
-    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
-    <Text>Este es el Home</Text>
+    <View style={{
+      flex:1,
+      alignItems:'center',
+      justifyContent:'center',
+      backgroundColor:'#76FF03',
+      }}>
+      <Image
+        source= {require("../assets/logo.png")}
+      />
+    {/* <Text>Este es el Home</Text> */}
     <View style={{marginTop:30}} >
     <Button
       title='Iniciar Sesión'
