e21eda26250fb1dfdcf5c8b17815f39d653b9d12
arielboyu <arielboyu@gmail.com>
2021-02-10 02:51:56 -0300

    fix(validacion pw,email)

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -14,21 +14,21 @@ export default function Login({ navigation }) {
   	useEffect(() => {
 		dispatch(getUsers());
     getUser()
-	},[]); 
+	},[]);
 
   let storageUser= "";
    // Trae el usuario guardado en asyncStorage, en forma de objeto.
-   const getUser = async () => {  
+   const getUser = async () => {
     try {
       const jsonData = await AsyncStorage.getItem('USER')
       console.log("JSON DATA ", jsonData)
       storageUser = jsonData;
       return jsonData != null ? JSON.parse(jsonData) : null;
-     
+
     } catch(e) {
       // error reading value
     }
-  } 
+  }
 
 	const [ data, setData ] = useState({
 		email                 : '',
@@ -39,45 +39,54 @@ export default function Login({ navigation }) {
 		isValidPassword       : true
 	});
 
-	const textInputChange = (val) => {
-		if (val.length != 0) {
+	function handlePasswordChange(val) {
+	  if (/^[a-zA-Z0-9]{4,20}$/i.test(val)){
 			setData({
-				...data,
-				email                 : val,
-				check_TextImputChange : true
+					...data,
+					password: val,
+					isValidPassword: true
 			});
-		} else {
+	  } else {
 			setData({
-				...data,
-				email                 : val,
-				check_TextImputChange : false
+					...data,
+					password: val,
+					isValidPassword: false
 			});
-		}
-	};
+	  }
+	}
 
-	const handlePasswordChange = (val) => {
-		setData({
-			...data,
-			password : val
-		});
-	};
 
-	/* const updateSecureTextEntry = () => {
-		setData({
-			...data,
-			secureTextEntry : !data.secureTextEntry
-		});
-	}; */
+			function textInputChange(val) {
+			  if (/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i.test(val)){
+					setData({
+							...data,
+							email: val,
+							check_textInputChange: true,
+							isValidUser: true
+					});
+			  } else {
+					setData({
+							...data,
+							email: val,
+							check_textInputChange: false,
+							isValidUser: false
+					});
+			  }
+			}
 
-	/* const onSubmit = (user) => {
-		dispatch(createNewUser(user));
-	}; */
+
+	const updateSecureTextEntry = () => {
+			setData({
+					...data,
+					secureTextEntry: !data.secureTextEntry
+			});
+	}
 
 	const handleLogin = () => {
 		if (data.isValidUser && data.isValidPassword) {
 			dispatch(login(data));
       storeUser(data)
-		} 
+		}
 	};
 
   //Guarda el user que se loguea en asyncStorage.
@@ -88,144 +97,144 @@ export default function Login({ navigation }) {
     } catch(e){
       console.log(e);
     }
-  } 
+  }
 
 
 
 	return (
 		<View style={styles.container}>
-			<View style={styles.logo}>
-				<ImageBackground style={{ width: 140, height: 140 }} source={require('../assets/LogoVector.png')} />
-			</View>
-			<View style={styles.icon_email}>
-				<FontAwesome name="user-o" color="black" size={28} />
-			</View>
-			<View>
-				<TextInput
-					label="Correo electrónico"
-					onChangeText={(text) => textInputChange(text)}
-					selectionColor="black"
-					type= 'outlined'
-					style={{
-						height      : 48,
-						paddingLeft : 5,
-						width       : 210,
-						marginTop:30,
-						marginLeft:100
-					}}
-				/>
-			</View>
-			<View
-				style={{
-					position : 'relative',
-					top      : 50
-				}}>
-				<View
-					style={{
-						marginTop:40
-					}}>
-					<View style={styles.icon_pw}>
-						<FontAwesome name="lock" color="black" size={30} />
-					</View>
-					<View>
-						<TextInput
-							label="Contraseña"
-							secureTextEntry={true}
-							type= 'outlined'
-							onChangeText={(text) => handlePasswordChange(text)}
-							selectionColor="black"
-							style={{
-								height      : 48,
-								paddingLeft : 5,
-								width       : 210,
-								position    : 'relative',
-								left        : 100,
-								top         : -90
-							}}
-						/>
-						{/* <TouchableOpacity onPress={updateSecureTextEntry}>
-							{data.secureTextEntry ? (
-								<Feather name="eye-off" color="grey" size={20} />
-							) : (
-								<Feather name="eye" color="grey" size={20} />
-							)}
-						</TouchableOpacity> */}
-					</View>
-					<View
-						style={{marginTop:-60
-						}}>
-						<Divider />
-						<Divider />
-					</View>
-				</View>
-			</View>
-			<View style={styles.botones}>
-				<View style={styles.boton}>
-						<Button
-							mode="contained"
-							onPress={handleLogin}
-							style={{
-								backgroundColor : '#006A34',
-								width           : '100%',
-							}}>
-							INICIAR SESIóN
-						</Button>
-				</View>
-        		<View style={styles.boton}>
-						<Button
-							mode="contained"
-							onPress={handleLogin}
-							style={{
-								backgroundColor : '#006A34',
-								width           : '100%',
-							}}>
-							INICIAR SESIóN
-						</Button>
-				</View>
-			</View>
-
+		<View style={styles.logo}>
+		<ImageBackground style={{ width: 140, height: 140 }}
+		source={require('../assets/LogoVector.png')} />
 		</View>
+		<View style={styles.action}>
+		<FontAwesome
+		 name="user-o"
+		 size={28}
+		 style={{marginRight:5,marginTop:5}}
+		 />
+		 <TextInput
+		 placeholder="Correo Electrónico"
+		 autoCapitalize="none"
+		 onChangeText={(val) => textInputChange(val)}
+		 onEndEditing={(e)=>handleValidUser(e.nativeEvent.text)}
+		 style={{
+		 height:48,
+		 paddingLeft:5,
+		 width:210,
+		 }}
+		 />
+		 {data.check_textInputChange ?
+		 <Animatable.View animation="bounceIn">
+		 <Feather
+		 name="check-circle"
+		 color="green"
+		 size={20}
+		 style={{marginTop:15}}
+		 />
+		 </Animatable.View>
+		 : null}
+		 </View>
+		 { data.isValidUser ? null :
+		 <Animatable.View animation="fadeInLeft" duration={500}>
+		 <Text style={styles.errorMsg}>Ingresa un e-mail valido</Text>
+		 </Animatable.View>
+		 }
+		 <View style={styles.action}>
+		 <Feather
+		 name="lock"
+		 size={28}
+		 style={{marginRight:5,marginTop:5}}
+		 />
+		 <TextInput
+		 placeholder="Contraseña"
+		 secureTextEntry={data.secureTextEntry ? true : false}
+		 autoCapitalize="none"
+		 onChangeText={(val) => handlePasswordChange(val)}
+		 style={{
+		 height:48,
+		 paddingLeft:5,
+		 width:210,
+		 }}
+		 />
+		 <TouchableOpacity
+		 onPress={updateSecureTextEntry}
+		 >
+		 {data.secureTextEntry ?
+		 <Feather
+		 name="eye-off"
+		 color="grey"
+		 size={20}
+		 style={{marginTop:15}}
+		 />
+		 :
+		 <Feather
+		 name="eye"
+		 color="grey"
+		 size={20}
+		 style={{marginTop:15}}
+		 />
+		 }
+		 </TouchableOpacity>
+		 </View>
+		 { data.isValidPassword ? null :
+		 <Animatable.View animation="fadeInLeft" duration={500}>
+		 <Text style={styles.errorMsg}>valores alfanumericos 8 caracteres min</Text>
+		 </Animatable.View>
+		 }
+		 <View
+		 style={{marginTop:60
+		 }}>
+		 <Divider />
+		 <Divider />
+		 <View style={styles.botones}>
+		 <View style={styles.boton}>
+		 <Button
+		 mode="contained"
+		 onPress={handleLogin}
+		 style={{
+		 backgroundColor : '#006A34',
+		 }}>
+		 INICIAR SESIóN
+		 </Button>
+		 </View>
+		 </View>
+	 </View>
+	</View>
 	);
-}
+};
+
 
 const styles = StyleSheet.create({
-	container   : {
-		backgroundColor : '#FFF',
-		height: '100%'
+container: {
+	backgroundColor:'#FFF',
+	height:'100%'
+},
+boton: {
+	alignItems: 'center',
+	marginTop:50,
+	marginLeft:10
+},
+iconButtons: {
+	marginBottom: 10,
+	borderRadius: 20,
+	marginTop: 25,
+	width: 15,
+	marginLeft: -12
+},
+logo: {
+	alignItems:'center',
+	marginTop:30
+},
+	action: {
+			flexDirection:'row',
+			marginTop:20,
+			marginLeft:90,
+			paddingBottom:5
 	},
-
-	icon_email  : {
-		top      : 65,
-		left     : 60
+	errorMsg: {
+			color: '#FF0000',
+			fontSize: 14,
+			marginLeft:120
 	},
-	icon_pw     : {
-		top      : -55,
-		left     : 62
-	},
-	botones    : {
-		display: 'flex',
-		alignItems: 'center',
-		width: '100%',
-		marginTop: 170
-	},
-	logo        : {
-		alignItems : 'center',
-		marginTop  : 30
-	},
-	boton       : {
-		alignItems : 'center',
-		marginTop  : -100,
-		marginLeft : 10
-	},
-	iconButtons : {
-		marginBottom : 10,
-		borderRadius : 20,
-		marginTop    : 25,
-		width        : 15,
-		marginLeft   : -12
-	},
-	heading     : {
-		fontSize   : 35,
-		alignItems : 'center'
-	}
 });
