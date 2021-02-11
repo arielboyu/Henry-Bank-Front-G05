a436aab73aa743d63d666878ee309eee25fd6e41
arielboyu <arielboyu@gmail.com>
2021-02-11 02:33:43 -0300

    fix(agregado checkbox)

--- screens/SendMoney.js
+++ screens/SendMoney.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import { ImageBackground,Image, StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { ImageBackground,Image, StyleSheet, Text, View,CheckBox } from 'react-native';
 import { Divider, Headline, Paragraph,TextInput, Button, } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
 import logo from '../assets/logo.png'
 import Transfer from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Picker } from '@react-native-picker/picker';
+
 
 export default function SendMoney({changeScreen}) {
+	const [isSelected, setSelection] = useState(false);
+
 	return (
     <View style={styles.container}>
     <View style={styles.heading}>
-    <Icon.Button 
-						name="arrow-left" 
+    <Icon.Button
+						name="arrow-left"
 						size={25}
 						color="black"
 						backgroundColor="#FFFF"
@@ -21,81 +25,82 @@ export default function SendMoney({changeScreen}) {
     </View>
     <View style={styles.logo}>
     <ImageBackground
-    style={{width:160,height:160}}
+    style={{width:60,height:60}}
     source={require('../assets/LogoVector.png')}
     >
     </ImageBackground>
     </View>
-      <View >
-      <TextInput
-      label="Nombre o e-mail"
-      mode="outlined"
-      selectionColor="black"
-      style={{height:40,
-        paddingLeft:5,
-        width:210,
-        position: 'relative',
-        left:80,
-        top:20
-      }}/>
-      </View>
-			<View style={{alignItems:'center',
-      backgroundColor:'#006A34',
-      width:80,
-      position:'relative',
-      top:36,
-      left:145,
-      height:36,
-      }}>
-			<Headline style={{color:'white'}}>$0</Headline>
-		  </View>
-			<View style={{position:'relative',
-      top:50,
-      left:20
-      }}>
-			<Text style={{width:350,
-        position:'relative',
-        left:10,
-        top:70
-      }}>
-      Acepto usar la sección de amigo solo con fines personales,
-      no comerciales </Text>
-			</View>
-      <View style={{position:'relative',
-      top:50
-      }}>
-      <View style={{position:'relative',
-      top:60,
-      }}>
-      <View style={styles.input_password}>
-      <TextInput
-      label="Enviar mensaje"
-      mode="outlined"
-      selectionColor="black"
-      style={{height:40,
-        paddingLeft:5,
-        width:210,
-        position: 'relative',
-        left:80,
-        top:-90}}
-      />
-      </View>
-      <View style={{
-      position: 'relative',
-      top:-20}} >
-      <Divider/>
-      <Divider/>
-      </View>
-      </View>
-      </View>
-			<View style={styles.boton}>
-			<View >
-			<Button style={styles.iconButtons}>
-			<Transfer name="send" size={30} color="#fff" />
-			</Button>
-			<Paragraph style={{fontWeight: '700'}}>Enviar</Paragraph>
-			</View>
-			</View>
+		<View style={styles.action}>
+		 <TextInput
+		 placeholder="Nombre o E-mail"
+		 autoCapitalize="none"
+		 style={{
+		 height:48,
+		 paddingLeft:5,
+		 width:180,
+		 }}
+		 />
+		 </View>
+		 <View style={styles.action}>
+ 		 <TextInput
+ 		 placeholder="Detalle de envio"
+ 		 autoCapitalize="none"
+ 		 style={{
+ 		 height:48,
+ 		 paddingLeft:5,
+ 		 width:180,
+ 		 }}
+ 		 />
+ 		 </View>
+		 <View style={{
+		 marginTop:15,
+		 marginLeft:80
+		 }} >
+		 <Picker
+			 style={{
+				 color:'black',
+				 width: 100,
+			 }}
+			 // selectedValue={data.form.typeID}
+			 // onValueChange={(val) => handleChange({ value: val, type: 'typeMoney' })}
+			 >
+			 <Picker.Item label="Pesos" value="Pesos" />
+			 <Picker.Item label="Dolar" value="Dolar" />
+			 </Picker>
+			 </View>
+		 <View style={styles.monto}>
+		 <TextInput
+		 placeholder="$ monto"
+		 autoCapitalize="none"
+		 keyboardType="decimal-pad"
+		 style={{
+		 height:40,
+		 paddingLeft:5,
+		 width:80,
+		 fontSize:12
+		 }}
+		 />
+		 </View>
+		 <View style={styles.checkboxContainer}>
+			 <CheckBox
+				 value={isSelected}
+				 onValueChange={setSelection}
+				 style={styles.checkbox}
+			 />
+		 </View>
+		 <Text>Acepto usar la sección amigo  con fines personales  {isSelected ? "👍" : "👎"}</Text>
+		 <View
+		 style={{marginTop:60
+		 }}>
+		 <View style={styles.botones}>
+		 <View style={styles.boton}>
+		 <Button style={styles.iconButtons}>
+	 	<Transfer name="send" size={30} color="#fff" />
+	 	</Button>
+	 	<Paragraph style={{fontWeight: '700',marginLeft:-36}}>Enviar</Paragraph>
+		 </View>
+		 </View>
+	 </View>
   </View>
 	)
 }
@@ -106,22 +111,34 @@ const styles = StyleSheet.create({
  		padding: 20,
  		backgroundColor: "#FFFF"
   	},
+		action: {
+				flexDirection:'row',
+				marginTop:20,
+				marginLeft:90,
+				paddingBottom:5
+		},
+		monto: {
+				flexDirection:'row',
+				marginTop:-40,
+				marginLeft:190,
+				paddingBottom:5
+		},
 logo: {
    alignItems:'center',
    marginTop:30,
 },
 boton: {
    alignItems:'center',
-   marginTop:75,
+   marginTop:15,
    marginLeft:20
 },
 iconButtons: {
   backgroundColor: '#006A34',
   marginBottom: 10,
   borderRadius: 20,
-  marginTop: 25,
+  marginTop: -65,
   width:15,
-  marginLeft:-12
+  marginLeft:-32
 },
   heading: {
     marginBottom: 10,
@@ -130,5 +147,6 @@ iconButtons: {
 		alignItems : 'center',
 		display: 'flex',
 		flexDirection: 'row'
-}
+},
+
 });
