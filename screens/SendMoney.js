ae604f901e6bd9320efc996663ad8d114fe72fc0
santiabo <santiabo@gmail.com>
2021-02-12 10:31:45 -0300

    third commit

--- screens/SendMoney.js
+++ screens/SendMoney.js
@@ -43,8 +43,8 @@ export default function SendMoney({ changeScreen, navigation }) {
    
   console.log("DATA 2 >>>", data.form)
       return  await axios.post(`http://${IP}:3001/movement/transferencia/${userId}`, data.form )
-      .then(  await axios.put(`http://${IP}:3001/account/${userId}`, data.form ))    
-      .then( changeScreen('main'))
+      .then(  await axios.put(`http://${IP}:3001/account/${userId}`, data.form ).catch((err) => alert(`No posee los fondos suficientes`)))    
+      .then( changeScreen('main'))          
       
            
     };
